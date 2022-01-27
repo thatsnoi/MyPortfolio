5c1732359c9e92ce40ebfe5e90afb06a5369fb44
Noah Jadallah <noah.jadallah@gmx.net>
2022-01-27 23:49:30 +0100

    mobile menu

--- components/Header.js
+++ components/Header.js
@@ -8,11 +8,15 @@ import {
   underlineEffect,
 } from './../styles/motion'
 import Link from 'next/link'
+import { useState } from 'react'
+import { MenuButton } from './MenuButton'
 
 export default function Header() {
+  const [expandedMenu, setExpandedMenu] = useState(true)
+
   return (
     <Container
-      className="fixed w-full flex justify-between text-3xl backdrop-blur-2xl h-16 dark:text-white dark:text-opacity-60 z-50"
+      className="fixed w-full flex justify-between items-center md:items-start text-3xl backdrop-blur-2xl h-16 dark:text-white dark:text-opacity-60 z-50"
       style={{
         background:
           'linear-gradient(180deg, #FFFFFF -17.13%, rgba(255, 255, 255, 0.61) 43.26%, rgba(255, 255, 255, 0) 100%)',
@@ -40,38 +44,53 @@ export default function Header() {
         <MenuItem href="/#projects"> Projects</MenuItem>
         <MenuItem href="/#contact">Contact</MenuItem>
       </motion.div>
+      <MenuButton
+        isOpen={expandedMenu}
+        onClick={() => setExpandedMenu(!expandedMenu)}
+        strokeWidth="3"
+        width="30"
+        color="white"
+      />
       <motion.div
-        className="group md:hidden flex flex-col items-end  space-y-2"
-        initial="hidden"
-        animate="visible"
-        variants={list()}
-        whileHover={{ scale: 1.05 }}
+        className="fixed top-0 right-0 left-0 bg-white dark:bg-black bg-opacity-90 z-40"
+        animate={{ height: expandedMenu ? '100vh' : '0vh' }}
+        transition={{ duration: 0.3 }}
+        style={{
+          backdropFilter: 'blur(100px)',
+        }}
       >
         <motion.div
-          className="w-10 h-1 bg-black dark:bg-white bg-opacity-70 dark:bg-opacity-80 group-hover:bg-opacity-100 group-hover:dark:bg-opacity-100"
-          variants={fadeIn}
-        ></motion.div>
-        <motion.div
-          className="w-6 h-1 bg-black dark:bg-white bg-opacity-70 dark:bg-opacity-80 group-hover:bg-opacity-100 group-hover:dark:bg-opacity-100"
-          variants={fadeIn}
-        ></motion.div>
-        <motion.div
-          className="w-8 h-1 bg-black dark:bg-white bg-opacity-70 dark:bg-opacity-80 group-hover:bg-opacity-100 group-hover:dark:bg-opacity-100"
-          variants={fadeIn}
-        ></motion.div>
+          className="flex flex-col p-4 items-center justify-center space-y-5 pt-20"
+          animate={expandedMenu ? 'visible' : 'hidden'}
+          variants={list(0)}
+        >
+          <MenuItem href="/#home" large>
+            Home
+          </MenuItem>
+          <MenuItem href="/#about" large>
+            About
+          </MenuItem>
+          <MenuItem href="/#projects" large>
+            {' '}
+            Projects
+          </MenuItem>
+          <MenuItem href="/#contact" large>
+            Contact
+          </MenuItem>
+        </motion.div>
       </motion.div>
     </Container>
   )
 }
 
-function MenuItem({ className, children, href }) {
+function MenuItem({ className, children, href, large }) {
   const router = useRouter()
 
   return (
     <Link href={href} passHref>
       <p className={className}>
         <motion.a
-          className={`relative ${
+          className={`relative ${large ? 'text-5xl' : ''} ${
             router.asPath === href
               ? 'font-bold standard-text-strong'
               : 'standard-text-light-hover'
---
+++ components/MenuButton.js
@@ -0,0 +1,94 @@
+import React from 'react'
+import { motion } from 'framer-motion'
+
+const MenuButton = ({
+  isOpen = false,
+  width = 30,
+  height = 30,
+  strokeWidth = 1,
+  color = '#000',
+  transition = null,
+  lineProps = null,
+  ...props
+}) => {
+  const variant = isOpen ? 'opened' : 'closed'
+  const top = {
+    closed: {
+      rotate: 0,
+      translateY: 0,
+    },
+    opened: {
+      rotate: 45,
+      translateY: 1.25,
+    },
+  }
+  const center = {
+    closed: {
+      opacity: 1,
+    },
+    opened: {
+      opacity: 0,
+    },
+  }
+  const bottom = {
+    closed: {
+      rotate: 0,
+      translateY: 0,
+    },
+    opened: {
+      rotate: -45,
+      translateY: -1.5,
+      translateX: 0.6,
+    },
+  }
+  lineProps = {
+    stroke: 'currentColor',
+    strokeWidth: strokeWidth,
+    vectorEffect: 'non-scaling-stroke',
+    initial: 'closed',
+    animate: variant,
+    transition,
+    ...lineProps,
+  }
+  const unitHeight = 4
+  const unitWidth = (unitHeight * width) / height
+
+  return (
+    <motion.svg
+      viewBox={`0 0 ${unitWidth} 4`}
+      overflow="visible"
+      preserveAspectRatio="none"
+      width={width}
+      height={height}
+      {...props}
+      className="md:hidden standard-text-light z-50"
+    >
+      <motion.line
+        x1="0"
+        x2="0.3rem"
+        y1="0"
+        y2="0"
+        variants={top}
+        {...lineProps}
+      />
+      <motion.line
+        x1="0.1rem"
+        x2="0.3rem"
+        y1="1.5"
+        y2="1.5"
+        variants={center}
+        {...lineProps}
+      />
+      <motion.line
+        x1="0"
+        x2="0.3rem"
+        y1="3"
+        y2="3"
+        variants={bottom}
+        {...lineProps}
+      />
+    </motion.svg>
+  )
+}
+
+export { MenuButton }
